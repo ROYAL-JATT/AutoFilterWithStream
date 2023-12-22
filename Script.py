11114dac00125ec0e72cde08c59c35c3d52240e6
Preet mehra <143023174+ROYAL-JATT@users.noreply.github.com>
2023-12-23 00:02:50 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -1,4 +1,4 @@
-lclass script(object):
+class script(object):
     START_TXT = """<b>Hᴇʟʟᴏ {},
 Mʏ Nᴀᴍᴇ Is <a href=https://t.me/{}>{}</a>, I Cᴀɴ Pʀᴏᴠɪᴅᴇ Mᴏᴠɪᴇs, Jᴜsᴛ Aᴅᴅ Mᴇ Tᴏ Yᴏᴜʀ Gʀᴏᴜᴘ Aɴᴅ Eɴᴊᴏʏ 😍</b>"""
 
