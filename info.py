28bd4a5e17c9a123bc26e2a140aeddbd515eb810
Preet mehra <143023174+ROYAL-JATT@users.noreply.github.com>
2024-03-07 22:42:55 +0530

    Update info.py

--- info.py
+++ info.py
@@ -95,8 +95,8 @@ else:
     ON_HEROKU = False
 BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', '0.0.0.0'))
 FQDN = str(getenv('FQDN', BIND_ADRESS)) if not ON_HEROKU or getenv('FQDN') else APP_NAME+'.herokuapp.com'
-URL = "https://vocal-flossi-maximum.koyeb.app/".format(FQDN) if ON_HEROKU or NO_PORT else \
-    "https://vocal-flossi-maximum.koyeb.app/".format(FQDN, PORT)
+URL = "https://vast-dawn-51288-0696eb086566.herokuapp.com/".format(FQDN) if ON_HEROKU or NO_PORT else \
+    "https://vast-dawn-51288-0696eb086566.herokuapp.com/".format(FQDN, PORT)
 SLEEP_THRESHOLD = int(environ.get('SLEEP_THRESHOLD', '60'))
 WORKERS = int(environ.get('WORKERS', '4'))
 SESSION_NAME = str(environ.get('SESSION_NAME', 'LazyBot'))
@@ -111,9 +111,9 @@ else:
     ON_HEROKU = False
 HAS_SSL=bool(getenv('HAS_SSL',False))
 if HAS_SSL:
-    URL = "https://vocal-flossi-maximum.koyeb.app/".format(FQDN)
+    URL = "https://vast-dawn-51288-0696eb086566.herokuapp.com/".format(FQDN)
 else:
-    URL = "https://vocal-flossi-maximum.koyeb.app/".format(FQDN)
+    URL = "https://vast-dawn-51288-0696eb086566.herokuapp.com/".format(FQDN)
 
 LOG_STR = "Current Cusomized Configurations are:-\n"
 LOG_STR += ("IMDB Results are enabled, Bot will be showing imdb details for you queries.\n" if IMDB else "IMBD Results are disabled.\n")
